8eb9607b698575b3393221ca39b6fbb1ba2da2ac
Adam Jurkiewicz Pythonista <adam@jurkiewicz.tech>
2023-12-19 10:39:09 +0100

    OOP

--- 02_wtorek/OOP_metody/oop_init.py
+++ 02_wtorek/OOP_metody/oop_init.py
@@ -3,18 +3,22 @@
 class Pracownik:
 
     def __init__(self, name, surname, year, place):
-        self.imie_nazwisko = name + surname
+        self.imie_nazwisko = f"{name} {surname}"
         self.year = year
         self.place = place
         self.gender = "M" if name[-1] != "a" else "F"
-        print("Init dla obiektu")
+        print(f"Init dla obiektu: {id(self)}")
 
     def show(self):
         print(f"Pracownik {self.imie_nazwisko}, zatrudniony na stanowisku {self.place} od roku {self.year}.")
 
 
 pracownik1 = Pracownik("Adam", "Jurkiewicz", 1999, "Administrator")
+pracownik2 = Pracownik("Beata", "Jurkiewicz", 1995, "Handlowiec")
 print("After")
-pracownik1.x = 30
+print(id(pracownik1))
+print(id(pracownik2))
 
-print(dir(pracownik1))
+pracownik1.show()
+pracownik2.show()
+
