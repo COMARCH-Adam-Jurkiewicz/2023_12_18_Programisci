be1da68e1a85a8cab99f5a75f8b59f0ea9349aeb
Adam Jurkiewicz Pythonista <adam@jurkiewicz.tech>
2023-12-19 10:42:12 +0100

    OOP - zdrobne zadanie

--- 02_wtorek/OOP_metody/oop_init.py
+++ 02_wtorek/OOP_metody/oop_init.py
@@ -12,6 +12,14 @@ class Pracownik:
     def show(self):
         print(f"Pracownik {self.imie_nazwisko}, zatrudniony na stanowisku {self.place} od roku {self.year}.")
 
+    def set_salary(self, new_salary_net: int):
+        # dodaj nową pensję miesięczną netto i brutto + 40%
+        pass
+
+    def show_salary(self):
+        # wypisz info o pensji netto i brutto
+        pass
+
 
 pracownik1 = Pracownik("Adam", "Jurkiewicz", 1999, "Administrator")
 pracownik2 = Pracownik("Beata", "Jurkiewicz", 1995, "Handlowiec")
