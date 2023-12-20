04878f4b5eb47a794fa2569bb7fd735943d1ad17
Adam Jurkiewicz Pythonista <adam@jurkiewicz.tech>
2023-12-20 13:59:41 +0100

    rozwiazanie

--- 03_Środa/00_aplikacja/functions/nbp_operations.py
+++ 03_Środa/00_aplikacja/functions/nbp_operations.py
@@ -1,4 +1,13 @@
 import requests
+from datetime import date, timedelta
+def oblicz_ilosc_dni(days=7):
+    try:
+        ile_dni = int(days)
+        if ile_dni < 0:
+            ile_dni *= -1
+    except:
+        ile_dni = 7
+    return timedelta(days=ile_dni if ile_dni <= 14 else 14)
 
 def get_values(code: str, last_date: str, days: str) -> list:
     """
@@ -8,4 +17,15 @@ def get_values(code: str, last_date: str, days: str) -> list:
     :param days: liczba dni
     :return: lista wartości waluty, np.: [4.234, 4.214]
     """
-    pass
+    last_date = date.fromisoformat(last_date)
+    start_date = last_date - oblicz_ilosc_dni(days)
+    api_link = f"https://api.nbp.pl/api/exchangerates/rates/A/{code}/{start_date}/{last_date}?format=JSON"
+    r = requests.get(api_link)
+    json_data: dict = r.json()
+    kursy_waluty = json_data["rates"]
+    wart = []
+    for element in kursy_waluty:
+        wart.append(element["mid"])
+
+    print(f" From {api_link} got {wart}.")
+    return wart
--- 03_Środa/00_aplikacja/kursy_app.py
+++ 03_Środa/00_aplikacja/kursy_app.py
@@ -1,11 +1,16 @@
 import PySimpleGUI as sg
+from time import sleep
 from functions.nbp_operations import get_values
+from functions.wykres import plotting
+
 
 
 layout = [
     [sg.Text('Sprawdzimy kursu waluty'), sg.Combo(['EUR', 'USD', 'CHF'])],
     [sg.Input(key="DATA", enable_events=True, readonly=True), sg.CalendarButton("Calendar", target="DATA", format="%Y-%m-%d")],
     [sg.Text('Ile dni wstecz (max 14)'), sg.InputText()],
+    [sg.Text("-----------------------------------------------")],
+    [sg.Output(size=(50, 6), key="-OUTPUT-")],
     [sg.Button('Ok'), sg.Button('KONIEC')]
          ]
 
@@ -17,7 +22,17 @@ while True:
     if event == sg.WIN_CLOSED or event == 'KONIEC':  # if user closes window or clicks cancel
         break
 
-    print(values)
+    if event == "Ok":
+        currency_values = get_values(values[0], values["DATA"], values[1])
+        title = f"Wykres kursów waluty {values[0]} z {values[1]} dni przed {values['DATA']}"
+        sleep(10)
+        print(title, currency_values, type(currency_values))
+        # plotting(title, currency_values,)
+
+
+
+
+
 
 
 
