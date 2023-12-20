5bf377674f5e273bffd932e22d4576c18d0115f8
Adam Jurkiewicz Pythonista <adam@jurkiewicz.tech>
2023-12-20 15:46:25 +0100

    snakemd

--- 03_Środa/00_aplikacja/kursy_app.py
+++ 03_Środa/00_aplikacja/kursy_app.py
@@ -3,13 +3,14 @@ from time import sleep
 from functions.nbp_operations import get_values
 from functions.wykres import plotting
 from functions.markdown import mdcreate
-
+from functions.docx import docxcreate
 
 
 layout = [
     [sg.Text('Sprawdzimy kursu waluty'), sg.Combo(['EUR', 'USD', 'CHF'])],
-    [sg.Input(key="DATA", enable_events=True, readonly=True), sg.CalendarButton("Calendar", target="DATA", format="%Y-%m-%d")],
-    [sg.Text('Ile dni wstecz (max 14)'), sg.InputText()],
+    [sg.Input(key="DATA", enable_events=True, readonly=True, size=(15,1)), sg.CalendarButton("Calendar", target="DATA", format="%Y-%m-%d", size=15)],
+    [sg.Text('Ile dni wstecz (max 14)'), sg.InputText(size=(5,1))],
+    [sg.Text('Nazwa pliku: png,md,docx'), sg.InputText(size=(15,1))],
     [sg.Text("-----------------------------------------------")],
     [sg.Output(size=(50, 6), key="-OUTPUT-")],
     [sg.Button('Ok'), sg.Button('KONIEC')]
@@ -26,12 +27,14 @@ while True:
     if event == "Ok":
         currency_values = get_values(values[0], values["DATA"], values[1])
         title = f"Wykres kursów waluty {values[0]} z {values[1]} dni przed {values['DATA']}"
-        sleep(10)
         # print(title, currency_values, type(currency_values))
+        filename = values[2]
         legenda_wykres = f"Waluta {values[0]}"
         leg_x, leg_y = "Legenda X", "Legenda Y"
-        plotting(title, currency_values, legenda_wykres, leg_x, leg_y)
-        # mdcreate()
+        plotting(title, currency_values, legenda_wykres, leg_x, leg_y, filename=f"{filename}.png")
+        mdcreate(legenda_wykres, values['DATA'], currency_values, filename, picture=f"{filename}.png" )
+        docxcreate(filename)
+
 
 
 
