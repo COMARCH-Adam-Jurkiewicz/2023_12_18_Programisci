9cc8f055ad3c39841e57fe339ce45763b9049a19
Adam Jurkiewicz Pythonista <adam@jurkiewicz.tech>
2023-12-21 11:53:05 +0100

    flask

--- 04_Czwartek/01_flask/funkcje_dodatkowe/page_check.py
+++ 04_Czwartek/01_flask/funkcje_dodatkowe/page_check.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 def check_web_page(adrress: str):
     scan_url = f"https://urlscan.io/api/v1/search/?q=filename:{adrress}"
@@ -6,7 +7,21 @@ def check_web_page(adrress: str):
     r = requests.get(scan_url)
 
     if r.status_code == 200:
-        dane = r.json()
-        result = dane['result']
-        return result
+        dane = r.json()["results"][0]
+        # print(f"{type(dane)} | {dane=}")
+        dane_res = dane["result"]
+        # print(f"---> {type(dane_res)} | {dane_res=}")
+        # for klucz in dane:
+        #     print(f"{type(klucz)} | {klucz=}")
+        #     print(f"{dane[klucz]=}")
+        #     print("---------")
+            # print(json.dumps(dane, indent=2))
+        # result = dane['result']
+        return dane_res
     return "xxxxxxxxxxxxx"
+
+if __name__ == "__main__":
+    print(check_web_page("blog.jurkiewicz.tech"))
+    print(check_web_page("wp.pl"))
+    print(check_web_page("tozipxc6.dreamwp.com"))
+    print(check_web_page("tozipxc6.dreamwp.com/wp-content/ih/ilo/mo/home/info.php"))
